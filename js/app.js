03a047f0867beca2b4571cb2fbd3df389b8845ad
Alexey Babushkin <007-99@mail.ru>
2020-01-23 21:44:51 +0300

    ДЗ первый урок

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let money;
+let income; 
+let addExpenses; 
+let deposit; 
+let mission; 
+let period;
+
+const myAlert = "Привет";
+const myConsole = "Привет из консоли";
+
+alert( myAlert );
+console.log( myConsole );
