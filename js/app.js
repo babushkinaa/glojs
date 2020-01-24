2d119b8b6ad4d2729496b0bbdaff89d48e448db9
Alexey Babushkin <007-99@mail.ru>
2020-01-24 09:18:24 +0300

    Промежуточный коммит

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
-let money;
-let income; 
-let addExpenses; 
-let deposit; 
-let mission; 
-let period;
+let money = 10000;
+let income = "Профи.ру"; 
+let addExpenses = "Интернет, Комунальные платежи, Школа, Сад, Транспорт"; 
+let deposit = true; 
+let mission = 1000000; 
+let period = 12;
 
-const myAlert = "Привет";
-const myConsole = "Привет из консоли";
 
-alert( myAlert );
-console.log( myConsole );
+console.log('money: ', typeof(money));
+console.log('income: ', typeof(income));
+console.log('deposit: ', typeof(deposit));
+
+console.log('Длинна строки addExpenses: ', addExpenses.length);
